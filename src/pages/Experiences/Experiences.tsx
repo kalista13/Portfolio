3ccf36448b2c9716cc84c14786e2237150c68e1a
kalista13 <kalistasingh4@gmail.com>
2025-09-14 15:26:09 -0600

    Chnages to Experiences and Contacts page

--- src/pages/Experiences/Experiences.tsx
+++ src/pages/Experiences/Experiences.tsx
@@ -8,81 +8,84 @@ export default function Experiences() {
 
       <div className={styles.timeline}>
         {/* Experience 1 */}
+
         <div className={styles.experienceItem}>
           <div className={styles.content}>
-            <h2 className={styles.jobTitle}>Mission Spacewalker</h2>
+            <h2 className={styles.jobTitle}>Software Co-Lead</h2>
             <p className={styles.role}>
-              Co-Lead Web Developer <span className={styles.date}>| 5/25 - Present</span>
+               Mission SpaceWalker<span className={styles.date}>| 9/24 - Present</span>
             </p>
             <ul className={styles.details}>
-              <li>Developed responsive front-end interfaces using React, TypeScript, JavaScript, and CSS.</li>
-              <li>Designed website layouts and user flows in Figma.</li>
+              <li>Co-lead software development for a cyanobacteria bioreactor project as a part of the Canadian Reduced Gravity Experiment Design Challenge (CAN-RGX).</li>
+              <li>Engineered a python/Tkinter dashboard display live camera feed, minute sensor data and control hardware.</li>
+              <li>Programmed and tested embedded software for sensors and system components, ensuring accurate data capture and performance.</li>
+              <li>Oversee smooth integration of software with hardware systems</li>
             </ul>
           </div>
         </div>
+
 
         {/* Experience 2 */}
         <div className={styles.experienceItem}>
           <div className={styles.content}>
-            <h2 className={styles.jobTitle}>Mission SpaceWalker</h2>
+            <h2 className={styles.jobTitle}>Web Developer Co-Lead</h2>
             <p className={styles.role}>
-              Software Co-Lead <span className={styles.date}>| 9/24 - Present</span>
+               Mission Spacewalker<span className={styles.date}>| 5/25 - Present</span>
             </p>
             <ul className={styles.details}>
-              <li>Co-lead software development for a cyanobacteria bioreactor in the Canadian Reduced Gravity Experiment Design Challenge (CAN-RGX).</li>
-              <li>Developed a Python/Tkinter dashboard to display live camera feed, monitor sensor data, and control hardware (flow sensors, pressure sensors, stepper motor).</li>
-              <li>Integrated software with hardware systems (Raspberry Pi, actuators, sensors) to ensure smooth operation.</li>
-              <li>Conducted testing of all components to verify data accuracy and system reliability.</li>
+              <li>Developed responsive front-end interfaces using React, Typescript, and CSS, improving project outreach and engagement..</li>
+              <li>Designed website layouts and user flows in Figma.</li>
+              <li>Integrated deployment workflows with GitHub Pages/Vercel.</li>
             </ul>
           </div>
         </div>
 
         <div className={styles.experienceItem}>
           <div className={styles.content}>
-            <h2 className={styles.jobTitle}>Fred Otto DiscoverE Program</h2>
+            <h2 className={styles.jobTitle}>Web Developer Associate</h2>
             <p className={styles.role}>
-              Curriculum Specialist <span className={styles.date}>| 9/25 - Present</span>
+               Network for Applied Tech<span className={styles.date}>| 1/25 - 5/25</span>
             </p>
             <ul className={styles.details}>
-              <li>Co-lead software development for a cyanobacteria bioreactor in the Canadian Reduced Gravity Experiment Design Challenge (CAN-RGX).</li>
-              <li>Developed a Python/Tkinter dashboard to display live camera feed, monitor sensor data, and control hardware (flow sensors, pressure sensors, stepper motor).</li>
-              <li>Integrated software with hardware systems (Raspberry Pi, actuators, sensors) to ensure smooth operation.</li>
-              <li>Conducted testing of all components to verify data accuracy and system reliability.</li>
+              <li>Built frontend features and contributed to full-stack development with Next.js, React, TypeScript, and SCSS</li>
+              <li>Collaborated with cross-functional teams to design and deploy scalable, responsive websites.</li>
             </ul>
           </div>
         </div>
 
         <div className={styles.experienceItem}>
           <div className={styles.content}>
-            <h2 className={styles.jobTitle}>Fred Otto DiscoverE Program</h2>
+            <h2 className={styles.jobTitle}>Curriculum Specialist </h2>
             <p className={styles.role}>
-              Instructor <span className={styles.date}>| 5/25 - 8/25</span>
+              Fred Otto DiscoverE Program<span className={styles.date}>| 9/25 - Present</span>
             </p>
             <ul className={styles.details}>
-              <li>Promoting STEM engagement through hands-on projects.</li>
-              <li>Instructing engineering focused summer camp sessions for youth as part of DiscoverE.</li>
+              <li>Applied computational thinking and problem-solving skills to develop innovative, interactive hands-on coding/engineering projects that introduces youth to computer science, engineering, and technology concepts.</li>
+              <li>Collaborated with a multidisciplinary team to create, test, and optimize educational content.</li>
             </ul>
           </div>
         </div>
 
         <div className={styles.experienceItem}>
           <div className={styles.content}>
-            <h2 className={styles.jobTitle}>Network for Applied Tech</h2>
+            <h2 className={styles.jobTitle}>Instructor </h2>
             <p className={styles.role}>
-               Web Developer Associate <span className={styles.date}>| 1/25 - 5/25</span>
+              Fred Otto DiscoverE Program<span className={styles.date}>| 5/25 - 8/25</span>
             </p>
             <ul className={styles.details}>
-              <li>Built front-end features and collaborated on full-stack development using Next.js, TypeScript, React, and CSS.</li>
-              <li>Improved site functionality and worked with teammates on design and deployment.</li>
+              <li>Delivered STEM summer camps introducing youth to coding, robotics, and engineering concepts</li>
+              <li>Promoted engagement in CS and technology through interactive, project-based learning.</li>
             </ul>
           </div>
         </div>
 
+        
+
         <div className={styles.experienceItem}>
           <div className={styles.content}>
-            <h2 className={styles.jobTitle}>AIESEC in Edmonton</h2>
+            <h2 className={styles.jobTitle}>Team Leader of Outgoing Exchange</h2>
             <p className={styles.role}>
-              Team Leader of Outgoing Exchange  <span className={styles.date}>| 10/23 - 5/25</span>
+              AIESEC in Edmonton<span className={styles.date}>| 10/23 - 5/25</span>
             </p>
             <ul className={styles.details}>
               <li>Implementing marketing strategies to recruit exchange participants.</li>
